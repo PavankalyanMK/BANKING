5ad956b63e6d5fc5fa53072a9b90345bf8a27404
PavankalyanMK <68377310+PavankalyanMK@users.noreply.github.com>
2021-09-15 20:33:39 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light" style="background-color:rgba(0, 0, 0, 0.6);">
+      <a class="navbar-brand" href="index.php" style="color : red;"><b>PAVAN BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar" >
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color :red;"><b>HOME</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : red;"><b>CREATE USER</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : red;"><b>TRANSFER MONEY</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : red;"><b>TRANSACTION HISTORY</b></a>
+              </li>
+          </div>
+       </nav>
+
+       
